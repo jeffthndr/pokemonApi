f27f55b8a15f61e432d3ccc06618ff3ce6f79b7a
Jeff <jeffersonasto95@gmail.com>
2023-11-29 19:46:49 +0100

    Add changes to App

--- ProyectoPokemon/src/App.tsx
+++ ProyectoPokemon/src/App.tsx
@@ -1,12 +1,12 @@
-
 function App() {
   return (
     <>
       <div>
         <h1>Poke Api</h1>
+        <h2>Este es un ejemplo para probar funciones</h2>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
